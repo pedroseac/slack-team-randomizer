b9bc682d1227abff89eff06681e4dcbe2ecda1b6
Pedro Seac <pv.seac@gmail.com>
2015-10-15 00:00:15 -0300

    Random members on message

--- main.js
+++ main.js
@@ -30,37 +30,63 @@ function shuffle(array) {
 var slack = new Slack(token, autoReconnect, autoMark);
 
 slack.on('open', function() {
-	var lunchGroupOrChannel = slack.getChannelByName('sampa');
-
-	var members = (function () {
-		var usersObject = slack.users;
-		var usersIds = lunchGroupOrChannel.members;
-		console.log(usersObject);
-		console.log(usersIds);
-		var results = [];
-		for (id in usersIds) {
-			console.log(id);
-			results.push(usersObject[usersIds[id]].name);
-		}
-		return results;
-	})();
-
 	console.log("Welcome to Slack. You are @" + slack.self.name + " of " + slack.team.name);
+});
 
-	members = shuffle(members);
-	var nr_groups = 3;
-	var nr_members = 4;
-	var nr_number = 1;
+slack.on('message', function(message) {
+	if (message.text && message.text.search(/randomize-team/) >= 0) {
+		var channel = message.text.match(/channel=["']([a-zA-Z\d]+)["']/);
+		var group = message.text.match(/group=["']([a-zA-Z\d]+)["']/);
+		var teamSize = message.text.match(/team-size=([a-zA-Z\d]+)/);
+		var nrTeams = message.text.match(/nr-teams=([a-zA-Z\d]+)/);
+		var purpose = message.text.match(/purpose=["']([a-zA-Z\d]+)["']/);
 
-	lunchGroupOrChannel.send('Vamos comer ae?');
-	lunchGroupOrChannel.send('------------------');
-	while(members.length > 0) {
-		lunchGroupOrChannel.send('Team ' + nr_number);
-		lunchGroupOrChannel.send(members.splice(0, Math.min(nr_members, members.length)).join(', '));
-		lunchGroupOrChannel.send('------------------');
-		nr_number++;
+		var target = slack.getChannelGroupOrDMByID(message.channel);
+
+		if (channel) {
+			target = slack.getChannelByName(channel[1]);
+		} else if (group) {
+			target = slack.getGroupByName(group[1]);
+		}
+
+		if (target) {
+			var members = (function () {
+				var usersObject = slack.users;
+				var usersIds = target.members;
+				var results = [];
+				for (id in usersIds) {
+					if(!usersObject[usersIds[id]].deleted) results.push(usersObject[usersIds[id]].name);
+				}
+				return results;
+			})();
+
+			members = shuffle(members);
+			// target.send('Hey! Someone requested teams?');
+			console.log('Hey! Someone requested teams?');
+			// if (purpose) target.send('Purpose: ', purpose);
+			if (purpose) console.log('Purpose: ', purpose[1]);
+
+			var nr_members = 4;
+
+			if (teamSize) {
+				nr_members = teamSize[1];
+			} else if (nrTeams) {
+				nr_members = Math.ceil(members.length / nrTeams[1]);
+			}
+
+			var i = 1;
+			while(members.length > 0) {
+				// target.send('Team ' + i);
+				console.log('TEAM ' + i);
+				// target.send(members.splice(0, Math.min(nr_members, members.length)).join(', '));
+				console.log(members.splice(0, Math.min(nr_members, members.length)).join(', '));
+				// target.send('------------------');
+				console.log('------------------');
+				i++;
+			}
+		}
+
 	}
-
 });
 
 slack.on('error', function(error) {
